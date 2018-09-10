57eb1295feed4b36fc381059b424ddbc5ec3cd3f
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-10 19:22:29 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+error_reporting(0);
+$angka = 5;
+
+//jawaban untuk soal 1 
+function soal1($a, $b){
+	if ($a > 0) {
+		if ($b > 0) {
+			echo "*";
+			$b--;
+			soal1($a, $b);
+		}
+		else{
+			echo "<br>";
+			$a--;
+			soal1($a, $a);
+		}
+	}
 }
-rekursif(5);
+soal1 ($angka, $angka);
+
+echo "==========================================";
+echo "<br>";
+
+//jawaban untuk soal 2 
+function soal2($a, $b, $angka){
+	if ($a <= $angka) {
+		if ($b <= $a) {
+			echo "$b";
+			$b++;
+			soal2($a, $b, $angka);
+		}
+		else{
+			echo "<br>";
+			$a++;
+			soal2($a, 1, $angka);
+		}
+	}
+}
+soal2 (1, 1, $angka);
+
 ?>
